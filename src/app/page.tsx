be938674b3344d6af1563bd673a52bb7d3671e2f
Noam Ben Simon <noampvb@gmail.com>
2024-07-23 16:50:24 -0400

    Updated pages to create new style and include Clerk.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,110 +1,46 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Input } from "@/components/ui/input"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
 import { useRouter } from "next/navigation"
-
-const schema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  phone: z.string().optional(),
-})
-
-export function UserForm() {
-
-  const form = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      name: "",
-      email: "",
-      phone: "",
-    },
-  });
-
-  const router = useRouter()
-
-  async function onSubmit(data: z.infer<typeof schema>) {
-    const response = await fetch("/api/users",
-      { method: "POST", body: JSON.stringify(data) }
-    ).then((res) => res.json())
-
-    if (response.message) {
-      return
-    }
-
-    const id = response.id
-
-    router.push(`/api/users/${id}`)
-  }
-
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>name</FormLabel>
-              <FormControl>
-                <Input placeholder="name goes here" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>email</FormLabel>
-              <FormControl>
-                <Input placeholder="email goes here" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="phone"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>phone number</FormLabel>
-              <FormControl>
-                <Input placeholder="phone optionally goes here" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-center">
-          <Button type="submit">Submit</Button>
-        </div>
-      </form>
-    </Form>
-  )
-}
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <main>
-      <div style={{ textAlign: "center", paddingTop: "5%" }}>Account Creation Page</div>
-      <div style={{ paddingTop: "2.5%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <UserForm />
-      </div>
+      <ClerkProvider>
+        <body>
+          <h3 style={{ textAlign: "center", paddingTop: "5%" }}>Welcome to my page</h3>
+          <br></br>
+          <h1 style={{ textAlign: "center", paddingTop: "5%" }}> This is a simple project that works with next.js, clerk, middleware, and prisma to simulate an extremely simple posting &quot;blog&quot; site.</h1>
+          <SignedOut>
+            <div style={{ textAlign: "center", paddingTop: "5%" }}>
+              <SignInButton>
+                <div style={{ textAlign: "center", paddingTop: "5%" }}>
+                  <Button>Sign In</Button>
+                </div>
+              </SignInButton>
+            </div>
+            <div style={{ textAlign: "center", paddingTop: "5%" }}>
+              Or
+              <div style={{ textAlign: "center", paddingTop: "5%" }}>
+                <Button onClick={() => router.push('/api')}>Create Account</Button>
+              </div>
+            </div>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </body>
+      </ClerkProvider>
     </main>
   );
 }
